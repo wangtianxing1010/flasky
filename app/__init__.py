0fba4ce6129411fca2bb6d28063b2e2c26c4ee45
wangtianxing1010 <16573740+wangtianxing1010@users.noreply.github.com>
2018-06-21 18:53:53 -0700

    .

--- app/__init__.py
+++ app/__init__.py
@@ -34,10 +34,10 @@ def create_app(config_name):
 		from flask_sslify import SSLify
 		sslify=SSLify(app)
 		
-	from main import main as main_blueprint
+	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
 	
-	from auth import auth as auth_blueprint
+	from .auth import auth as auth_blueprint
 	app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
 	from .api_1_0 import api as api_1_0_blueprint
