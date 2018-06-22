94711c44ffeeb1bcdb23eae5ffc4d00124646ad3
wangtianxing1010 <16573740+wangtianxing1010@users.noreply.github.com>
2018-06-22 03:24:57 -0700

    mail config

--- config.py
+++ config.py
@@ -15,6 +15,8 @@ class Config:
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
+	MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')    
     
 	@staticmethod
 	def init_app(app):
